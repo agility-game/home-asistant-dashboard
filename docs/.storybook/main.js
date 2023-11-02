45fe9768d9fbe18f200c58b727df6b30ada12360
Willem van Heemstra <wvanheemstra@icloud.com>
2023-11-02 16:04:23 +0000

    Update simple component

--- docs/.storybook/main.js
+++ docs/.storybook/main.js
@@ -6,7 +6,8 @@ const config = {
   addons: [
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@storybook/addon-interactions")
+    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-a11y")
   ],
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
